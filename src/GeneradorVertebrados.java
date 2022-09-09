1fd395acc1c770afa9baab2bca10242091476614
yulyber <yberbesi@uninorte.edu.co>
2022-09-08 20:20:37 -0500

    ok

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,6 +4,9 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[3] = new Vertebrado("Pez dorado", "Pez"); 
+        miVertebrados[4] = new Vertebrado("Cirujano", "Pez"); 
+        miVertebrados[5] = new Vertebrado("Perro", "Mamifero"); 
         
         return miVertebrados;
     }
